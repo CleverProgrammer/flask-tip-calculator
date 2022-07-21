e48505420fabc52032781ff597399d80108ba036
Rafeh Qazi <qazi@cleverprogrammer.com>
2022-07-21 18:17:35 +0000

    fetch request with python / pyscript now works

--- static/script.py
+++ static/script.py
@@ -1,5 +1,10 @@
 from pyodide.http import pyfetch
-print('hi PYSCRIPT is working?')
-response = await pyfetch("/background_process_test")
-data = await response.json()
-print(data)
+import asyncio
+
+async def get_data():
+  print('hi PYSCRIPT is working?')
+  response = await pyfetch("/background_process_test")
+  # data = await response.json()
+  print(response)
+
+print(await get_data())
