b58591e2c48af71da9b4553e77b025a3e5049db3
Rafeh Qazi <qazi@cleverprogrammer.com>
2022-07-21 20:10:58 +0000

    ✅ Final - Get Random Dog. This has server + client side code. Database. Conditional rendering. API Requests... and routing

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
+from replit import db
 import requests
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
-
-
-# background process happening without any refreshing
-@app.route('/get_dog', methods=['GET', 'POST'])
-def get_dog():
-    print("woof woof! 🐶")
-    return "woof woof! 🐶"
-
-
-@app.route('/increase_people')
-def increase_people():
-    print('increase')
-
+    db['dog_images_generated'] = db.get('dog_images_generated', 0) + 1
+    response = requests.get("https://dog.ceo/api/breeds/image/random")
+    data = response.json()
+    return render_template('index.html', dog_image=data['message'], dogs_generated=db['dog_images_generated'])
+    
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=81)
